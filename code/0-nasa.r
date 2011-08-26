76d58dfba8c64f15d4acdfda6f2ef579c1b2b33b
hadley <h.wickham@gmail.com>
2011-08-26 10:00:36 -0500

    Unify Hadley and Heike's mapping code

--- code/0-nasa.r
+++ code/0-nasa.r
@@ -15,19 +15,40 @@ if (!file.exists("nasadata.rds")) {
   nasa <- readRDS("nasadata.rds")
 }
 
-outlines <- as.data.frame(map("world", plot = FALSE,
-  xlim = -c(113.8, 56.2), ylim = c(-21.2, 36.2))[c("x","y")])
+getbox <- function (map, xlim, ylim) {
+	# identify all regions involved
+	small <- subset(map, (long > xlim[1]) & (long < xlim[2]) & (lat > ylim[1]) & (lat < ylim[2]))
+	regions <- unique(small$region)
+	small <- subset(map, region %in% regions)	
+	
+	# now shrink all nodes back to the bounding box
+	small$long <- pmax(small$long, xlim[1])
+	small$long <- pmin(small$long, xlim[2])
+	small$lat <- pmax(small$lat, ylim[1])
+	small$lat <- pmin(small$lat, ylim[2])
+
+	return(small)
+}
+
+world <- map_data("world")
+nasa <- getbox(world, xlim = c(-114.93, -55.07), ylim = c(-21.53, 36.64))
+# Remove slivvers
+
+nasa <- ddply(nasa, "group", function(df) {
+  if (diff(range(df$long)) < 1e-6) return(NULL)
+  if (diff(range(df$lat)) < 1e-6) return(NULL)
+  df
+})
+
 map <- list(
-  geom_path(aes(x, y), inherit.aes = FALSE, data = outlines, alpha = 0.4,
-    legend = FALSE),
-  scale_x_continuous(breaks = NA),
-  scale_y_continuous(breaks = NA), 
+  geom_polygon(aes(long, lat, group = group), inherit.aes = FALSE, 
+    data = nasa, legend = FALSE, fill = "grey80", colour = "white"),
+  scale_x_continuous(breaks = NA, expand = c(0.02, 0)),
+  scale_y_continuous(breaks = NA, expand = c(0.02, 0)), 
   xlab(NULL),
-  ylab(NULL),
-  coord_cartesian(xlim = c(-114.93, -55.07), ylim = c(-21.53, 36.64), wise = T))
+  ylab(NULL))
 
 
-tl 
 map_mini <- c(map, list(
   coord_cartesian(xlim = c(-114.93, -85), ylim = c(7.55, 36.64), wise = T)))
 
--- code/1-heike-scatter.R
+++ code/1-heike-scatter.R
@@ -1,32 +1,5 @@
 library(ggplot2)
 library(maps)
-
-getbox <- function (map, xlim, ylim) {
-	# identify all regions involved
-	small <- subset(map, (long > xlim[1]) & (long < xlim[2]) & (lat > ylim[1]) & (lat < ylim[2]))
-	regions <- unique(small$region)
-	small <- subset(map, region %in% regions)	
-	
-	# now shrink all nodes back to the bounding box
-	small$long <- pmax(small$long, xlim[1])
-	small$long <- pmin(small$long, xlim[2])
-	small$lat <- pmax(small$lat, ylim[1])
-	small$lat <- pmin(small$lat, ylim[2])
-
-	return(small)
-}
-
-world <- map_data("world")
-nasa <- getbox(world, xlim = c(-114.93, -55.07), ylim = c(-21.53, 36.64))
-
-
-map <- list(
-  geom_polygon(aes(long, lat, group=group), inherit.aes = FALSE, data = nasa, fill="grey80", 
-    legend = FALSE),
-  scale_x_continuous(breaks = NA),
-  scale_y_continuous(breaks = NA), 
-  xlab(NULL),
-  ylab(NULL),
-  coord_cartesian(xlim = c(-114.93, -55.07), ylim = c(-21.53, 36.64), wise = T))
+source("0-nasa.r")
 
 ggplot() + map + geom_point(aes(gx, gy), data = temp, shape = ".")

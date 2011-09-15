2038efea6d9be48a29d71f7840f09a0c651b4e67
Dianne Cook <visnut@gmail.com>
2011-09-15 14:28:03 -0500

    tinkered with templates

--- code/2-examples.R
+++ code/2-examples.R
@@ -1,17 +1,20 @@
 amp <- 30
 mu <- 330
 
-t <- seq(0, pi, length=12)
+t <- seq(0, pi, length=36)
 x1 <- mu+amp*sin(t)
 x2 <- mu+30-amp*sin(t)
-x3 <- seq(330,360, length=12)
+x3 <- seq(330,360, length=36)
+x4 <- amp*sin(t*6)
 
-locs <- data.frame(cbind(rep(t,3), c(x1,x2,x3), rep(1:3, each=12), rep(1,3*12)))
+locs <- data.frame(cbind(rep(t,4), c(x1,x2,x3,x4), rep(1:4, each=36), rep(1,4*12)))
 names(locs) <- c("time","temp","x_max", "y_max")
 
 locs2 <- data.frame(cbind(rep(t+pi,3), rep(c(mean(x1),mean(x2),mean(x3)), each=12), rep(1:3, each=12), rep(1,3*12)))
 names(locs2) <- c("time","temp","x_max", "y_max")
 
+library(ggplot2)
+qplot(time,temp, data=subset(locs, x_max==1), geom="line")
 
 # multiple lines
 locs <- glyphs(locs, "x_max", "time", "y_max", "temp") 
